7509d7a39930894f8b86a54752b22112769a031d
shiva <shiva@nimapinfotech.com>
2025-04-15 10:41:05 +0530

    report Cards done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Date Range Selector
   flatpickr("#datetime-picker", {
     mode: "range",
     dateFormat: "F, Y",
@@ -71,4 +72,76 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#datetime-picker")._flatpickr.clear();
     document.querySelector(".dates_selected").style.display = "none";
   });
+
+  // Collection report Cards
+  const reports = [
+    {
+      reportName: "Contract Mfg.",
+      amount: "₹ 3,00,000",
+      style: {
+        color: "#1E508D",
+        background: "#E6F1FF",
+      },
+    },
+    {
+      reportName: "Direct Govt. Inst.",
+      amount: "₹ 4,00,000",
+      style: {
+        color: "#FF3A6F",
+        background: "#FFEBE3",
+      },
+    },
+    {
+      reportName: "Indirect Govt. Inst.",
+      amount: "₹ 5,00,000",
+      style: {
+        color: "#A7B817",
+        background: "#FAFFCF",
+      },
+    },
+    {
+      reportName: "PCD Sales",
+      amount: "₹ 6,00,000",
+      style: {
+        color: "#FF9504",
+        background: "#FFEDD3",
+      },
+    },
+    {
+      reportName: "Ethical Sales",
+      amount: "₹ 7,00,000",
+      style: {
+        color: "#F6D913",
+        background: "#FFFCE7",
+      },
+    },
+    {
+      reportName: "Direct Export",
+      amount: "₹ 8,00,000",
+      style: {
+        color: "#0BB1B7",
+        background: "#D9FEFF",
+      },
+    },
+    {
+      reportName: "Deemed Export",
+      amount: "₹ 9,00,000",
+      style: {
+        color: "#556179",
+        background: "#E9E9E9",
+      },
+    },
+  ];
+
+  const allCards = document.getElementById("all-cards");
+  allCards.innerHTML = reports
+    .map(
+      (
+        report
+      ) => `<div style="background-color: ${report.style.background}" style class="report-card">
+      <p style="border-bottom: 2px solid ${report.style.color}">${report.reportName}</p>
+      <h2>${report.amount}</h2>
+    </div>`
+    )
+    .join("");
 });
